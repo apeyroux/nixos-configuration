742fe550b23369be94ab12edadc2a728474b4d6c
Alexandre Peyroux <alex@px.io>
2021-02-11 08:42:50 +0100

    snap

--- p53/configuration.nix
+++ p53/configuration.nix
@@ -198,6 +198,7 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
+  programs.adb.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
